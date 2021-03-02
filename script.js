c694b8bef9931a34a4c60a598db587034d92209b
Jack Barratt <barratt_jack@yahoo.com>
2021-03-02 14:46:11 +0000

    Update script.js
    calculator adjustments. code feels a bit like spaghetti but time to move on

--- script.js
+++ script.js
@@ -2,31 +2,87 @@ const display = document.getElementById("display-numbers");
 const numpad = document.querySelectorAll(".numpad");
 const operatorDiv = document.querySelectorAll(".operator");
 const equals = document.querySelector(".equals");
-
+const ac = document.querySelector(".ac");
+const percent = document.querySelector(".percent");
 
 let num1 = null;
-let num2 = null
-let operand = null;
-let displayValue = null;
+let num2 = null;
+let num3 = null;
+let operand;
+let currentVal = "";
+
+window.addEventListener("keydown", (e) => {
+    const btn = document.querySelector(`.buttons[data-key="${e.keyCode}"]`);
+    let numpad = btn.dataset.num;
+    enterNumbers(numpad);
+})
 
 numpad.forEach((numval) => {
     numval.addEventListener("click", (e) => {
-        display.innerHTML = e.target.dataset.key;
-        num2 = parseInt(e.target.dataset.key);
+        let numButton = e.target.dataset.num;
+        enterNumbers(numButton);
     })
 })
 
-operatorDiv.forEach((operand) => {
-    operand.addEventListener("click", (e) => {
-        num1 = num2;
-        operand = e.target.dataset.key;
-        console.dir(e.target.dataset.key);
+function enterNumbers(e) {
+    if (e === "." && currentVal.includes(".")) {
+        return false;
+    } else {
+        currentVal += e;
+        num2 = parseFloat(currentVal);
+        screenOutput(currentVal);
+    }
+}
+
+operatorDiv.forEach((operator) => {
+    operator.addEventListener("click", (e) => {
+        if (!operand) {
+            currentVal = "";
+            num1 = num2;
+            operand = e.target.dataset.num;
+        } else {
+            currentVal = "";
+            num1 = operate(operand, num1, num2);
+            screenOutputNum(num1);
+            operand = e.target.dataset.num;
+        }
     })
 })
 
 equals.addEventListener("click", () => {
-    display.innerHTML = operate(operand, num1, num2);
+    if (!num1){
+        screenOutputNum(num2);
+    } else {
+        num2 = operate(operand, num1, num2)
+        screenOutputNum(num2);
+        operand = null;
+    }
 })
+
+ac.addEventListener("click", () => {
+    display.innerHTML = 0;
+    num1 = null;
+    num2 = null;
+    operand = null;
+    displayValue = null;
+    currentVal = "";
+})
+
+function screenOutput(value) {
+    if(value.length > 17) {
+        display.innerHTML = value.substring(0, 17);
+    } else {
+        display.innerHTML = value;
+    }
+}
+
+function screenOutputNum(number) {
+    if (number.toString().length > 17) {
+        display.innerHTML = number.toExponential(11);
+    } else {
+        display.innerHTML = number;
+    }
+}
 
 function operate(op, num1, num2) {
     if (op === "add") {
@@ -36,9 +92,13 @@ function operate(op, num1, num2) {
     } else if (op === "multiply") {
         return num1 * num2;
     } else if (op === "divide") {
-        return (num1 / num2);
+        if (num2 === 0) {
+        return "Can't do that";
+        }
+        else {
+            return (num1 / num2);
+        }
     } else {
-        console.log(op, num1, num2)
-        return "Invalid";
+        return num2;
     }
 }
